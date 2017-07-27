9c85d2cfc3bed933af8010cd2123a076da20f983
Zakir <Zakir@LAPTOP-GQEFR3RC>
2017-07-27 11:45:02 -0400

    Tested the deal method, updated the program class to include comments for the Lab25

--- COMP123-S2017-Lesson11A/Program.cs
+++ COMP123-S2017-Lesson11A/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
  * Name: Shakil Hosin
  * Date: July 25, 2017
  * Description: Demo for Lesson 11
- * Version: 0.3 - Fixed a bug - non- implemented _initialize method
+ * Version: 0.4 - Tested the deal method, updated the program class to include comments for the Lab25
  */
 
 namespace COMP123_S2017_Lesson11A
@@ -27,8 +27,18 @@ namespace COMP123_S2017_Lesson11A
             Console.WriteLine();
 
             //hand = deck. deal5
+            /*
             hand.Add(deck.Deal1());// deal one card from the top of the deck
             Console.WriteLine(hand.ToString());
+
+            hand.Add(deck.Deal1());// deal one card from the top of the deck
+            Console.WriteLine(hand.ToString());
+
+            hand.Add(deck.Deal1());// deal one card from the top of the deck
+            Console.WriteLine(hand.ToString());
+            */
+             
+            // hand = deck.deal5(); //creates a new hand obkect and assigns it to hand. 
         }
     }
 }
